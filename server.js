d6b91ac0380b7b4e8de43724f82eaf4ba5f182b7
dreamlabo <toddlabo@gmail.com>
2019-03-22 09:12:46 -0600

    Working on Oauth

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
 const express = require('express');
+const passport = require('passport');  // how to handle authentication
+const GoogleStrategy = require('passport-google-oauth20').Strategy; // which auth stategy to use
+
 const app = express();
+// Client ID  663858297510-adv1b2rsfqjse556thsslgb9qr2os0rr.apps.googleusercontent.com
+// "client_secret":  ifYPMOhteI6EFx49h61ievSX
 
-app.get('/',  (req, res) => {
-    res.send({bye: 'buddy'});
-});
+passport.use(new GoogleStrategy()); // Creates a new instance of the Google passport stategy
+                                    // passport.use => I want to use GoogleStrategy
 
-
+//663858297510-adv1b2rsfqjse556thsslgb9qr2os0rr.apps.googleusercontent.com
 
 const PORT = process.env.PORT || 5000;  // defines the port as either the port given to us by Heroku
                                         // Or it's port 5000 on out local machine
