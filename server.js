819a62e20ff117bab489918994f8d3de1f44b42b
dreamlabo <toddlabo@gmail.com>
2019-03-21 21:49:01 -0600

    Second commit

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+
+app.get('/',  (req, res) => {
+    res.send({hi: 'there'});
+});
+
+
+
+const PORT = process.env.PORT || 5000;  // defines the port as either the port given to us by Heroku
+                                        // Or it's port 5000 on out local machine
+
+app.listen(PORT);                       // the app should listen on one of the const PORT we just defined
+
+//app.listen(process.env.PORT || 5000); // Here, we just combine the two lines of code.
