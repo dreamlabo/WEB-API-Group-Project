bde0e1c6c9250dc277f41252fefcfceb8baa7434
dreamlabo <toddlabo@gmail.com>
2019-03-29 14:36:00 -0600

    Working on Oauth - 2

--- server.js
+++ server.js
@@ -1,34 +1,11 @@
 const express = require('express');
-const passport = require('passport');  // how to handle authentication
-const GoogleStrategy = require('passport-google-oauth20').Strategy; // which auth stategy to use
-const keys = require('./config/Keys.js');
+require('./services/passport');
+require('./routes/authRoutes');
+
 
 const app = express();
 
-
-passport.use(new GoogleStrategy({            // Creates a new instance of the Google passport stategy
-    clientID: keys.googleClientID,           // passport.use => I want to use GoogleStrategy
-    clientSecret: keys.googleClientSecret,   // Arguments needed to get permission from google
-    callbackURL: '/auth/google/callback'     // route the user is sent to after google grants permission
-    },
-    (accessToken, refreshToken, profile, done) => {
-        console.log('access token', accessToken);
-        console.log('refresh token', refreshToken);
-        console.log('profile:', profile);
-    }
-    )
-);
-
-
-app.get('/auth/google', passport.authenticate('google', {scope: ['profile', 'email']
-}));   // attempt to authorize the user coming into this route
-       // 'google' > assigns the string 'google' to the strategy we are using in passport.use seen above
-       // scope: gives us access to username and passport
-
-app.get('/auth/google/callback', passport.authenticate('google'));
-
-
-
+require('./routes/authRoutes')(app);
 
 const PORT = process.env.PORT || 5000;  // defines the port as either the port given to us by Heroku
                                         // Or it's port 5000 on out local machine
---
+++ services/passport.js
@@ -0,0 +1,18 @@
+const passport = require('passport');  // how to handle authentication
+const GoogleStrategy = require('passport-google-oauth20').Strategy; // which auth stategy to use
+const keys = require('../config/Keys.js');
+
+
+
+passport.use(new GoogleStrategy({            // Creates a new instance of the Google passport stategy
+        clientID: keys.googleClientID,           // passport.use => I want to use GoogleStrategy
+        clientSecret: keys.googleClientSecret,   // Arguments needed to get permission from google
+        callbackURL: '/auth/google/callback'     // route the user is sent to after google grants permission
+    },
+    (accessToken, refreshToken, profile, done) => {
+        console.log('access token', accessToken);
+        console.log('refresh token', refreshToken);
+        console.log('profile:', profile);
+    }
+    )
+);
