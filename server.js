7fbe40049e4055d6300e6bda2c82a5fe6291ac28
dreamlabo <toddlabo@gmail.com>
2019-04-17 22:38:20 -0600

    Got Google Auth working (I think) Added the start of a "cookie" Added firstname/lastname/email to userSchema

--- server.js
+++ server.js
@@ -1,9 +1,26 @@
 const express = require('express');
+const mongoose  = require('mongoose');
+const cookieSession = require('cookie-session');   // give us access to cookies
+const passport = require('passport');   // make passport make use of cookies
+const keys = require('./Config/Keys');
+require('./models/User');     // order of your require statements matter
 require('./services/passport');
+
 //require('./routes/authRoutes');
 
-
+//mongoose.connect('keys.mongoURI', { useNewUrlParser: true } );
+//mongoose.connect('Keys.mongoURI');   // need the actual address when I actually make the mongoDB
 const app = express();
+
+app.use(
+    cookieSession({
+        maxAge: 30 * 24 * 60 * 60 * 1000,       // how long the cookie will last in the browser( in milliseconds)
+                                                // In this case => 30(days) * 24(hours) * 60(minutes) * 60(seconds) * 1000(millisec) (total of 30 days)
+        keys: [keys.cookieKey]
+    })
+);
+app.use(passport.initialize());
+app.use(passport.session());
 
 require('./routes/authRoutes')(app);
 
