c01b68c3bd7b3c1bd689c919f31363c136ea1ad8
dreamlabo <toddlabo@gmail.com>
2019-04-19 10:51:27 -0600

    Added prod keys

--- services/passport.js
+++ services/passport.js
@@ -26,7 +26,8 @@ passport.use(new GoogleStrategy(                 // Creates a new instance of th
     {
         clientID: keys.googleClientID,           // passport.use => I want to use GoogleStrategy
         clientSecret: keys.googleClientSecret,   // Arguments needed to get permission from google
-        callbackURL: '/auth/google/callback'     // route the user is sent to after google grants permission
+        callbackURL: '/auth/google/callback',     // route the user is sent to after google grants permission
+        proxy: true
     },
 
     (accessToken, refreshToken, profile, done) => {
