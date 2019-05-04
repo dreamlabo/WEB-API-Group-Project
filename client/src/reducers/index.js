2f98066f2fe64b85321a134e25fad25a2f8beb09
toddl <Toddlabo@gmail.com>
2019-05-04 12:47:10 -0600

    reverting2

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,13 +1,8 @@
 import { combineReducers } from "redux";
 import { reducer as reduxForm } from 'redux-form';
 import authReducer from './authReducer';
-import surveysReducer from './surveysReducer';
-import charityReducer from './charityReducer';
-
 
 export default combineReducers({
     auth: authReducer,
-    form: reduxForm,
-    surveys: surveysReducer,
-    charity: charityReducer
+    form: reduxForm
 });
